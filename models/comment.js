3b380ac0efc4d615740613bd67facc2b5f0e6d33
Aisha Osman <aisha.osman09@gmail.com>
2021-12-22 09:30:54 -0600

    model/comments

--- models/comment.js
+++ models/comment.js
@@ -7,7 +7,8 @@ Comment.init({
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        allowNull: false
     },
     user_id: {
         type: DataTypes.INTEGER,
