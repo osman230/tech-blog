1b759707923fc62739c407d0f8451b6b41b2babe
Aisha Osman <aisha.osman09@gmail.com>
2021-12-25 17:34:12 -0600

    update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,7 +8,7 @@ const router = require('express').Router();
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
-        res.redirect('/');
+        res.render('/');
         return;
     }
     res.render('login');
