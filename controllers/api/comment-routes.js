c1b2b3c9916985833667432327b4db1521cae37b
Aisha Osman <aisha.osman09@gmail.com>
2021-12-27 11:32:13 -0600

    .

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,34 +1,36 @@
 const router = require('express').Router();
-const { builtinModules } = require('module');
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// create a Comment
 router.post('/', withAuth, (req, res) => {
-    if(req.session) {
-        Comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
-            user_id: req.session.user_id,
-        }).then(commentData => res.json(commentData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err)
-        })
-    }
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
+// delete a comment
 router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
         where: {
             id: req.params.id
         }
-    }).then(commentData => {
-        if(!commentData) {
-            res.status(404).json({ message: 'Error: Please enter value info'});
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
             return;
         }
-        res.json(commentData);
-    }).catch(err => {
+        res.json(dbCommentData);
+    })
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
